969e936a9a34115a52c0fa260f228a5e5b681909
OmariLawrence <originalolawrence@gmail.com>
2020-01-20 15:01:03 -0400

    test

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -18,9 +18,9 @@ function Projects() {
                 <br/>
                 <h2>Eggcellence 2: Hard Boiled</h2>
                 <img src={egg} alt='egg'height="20%" width="20%"/>
-                <p>A silly game where you run around a village throwing eggs at 
+                <p>A very silly game where you run around a village throwing eggs at 
                     people and there houses, while trying to avoid being caught by
-                    the villagers. 
+                    the villagers.
                     <br></br>
                     <b><a href= "https://jormsgames.github.io/">Play Here!</a></b>
                 </p>
